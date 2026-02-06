516c1f1b9e6618373d6548e320f0e7bc8d1bc96c
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2026-02-07 00:39:15 +0700

    feat(config): update app configuration and add expo-asset dependency

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -4,6 +4,22 @@ const { withUniwindConfig } = require("uniwind/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add asset resolution
+config.resolver.assetExts.push(
+	// Image formats
+	"png",
+	"jpg",
+	"jpeg",
+	"gif",
+	"webp",
+	"svg",
+	// Font formats
+	"ttf",
+	"otf",
+	"woff",
+	"woff2",
+);
+
 const uniwindConfig = withUniwindConfig(config, {
 	cssEntryFile: "./global.css",
 	dtsFile: "./uniwind-types.d.ts",
